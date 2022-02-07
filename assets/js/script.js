534e9aeef2bef2d1ef37b5d5a6874001cc9f9ea2
mac Ray <rztechdude@yahoo.com>
2022-02-06 22:24:18 -0500

    added html time blocks and started script

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var tasks = {
+    timeblock: "",
+    description: "",
+};
+// invoke current date to DOM
+var currDate = moment().format("DD/MM/YYYY");
+$("#currentDay").text( "Today's date: " + currDate );
+
+function addTasks() {
+    var taskAdd = "";
+
+    $(".tasks").on( "click", );
+    
+    console.log();
+}
+function saveTask() {
+
+}
+addTasks();
